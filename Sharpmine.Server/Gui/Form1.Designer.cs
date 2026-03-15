e2ae03eec1a15961d349645487b7a9eff5c43ce3
Spi3lot <spi3lot@gmail.com>
2026-03-15 05:10:38 +0100

    Adapt form to new logging

--- Sharpmine.Server/Gui/Form1.Designer.cs
+++ Sharpmine.Server/Gui/Form1.Designer.cs
@@ -32,36 +32,55 @@ partial class Form1
     {
         listBoxClients = new System.Windows.Forms.ListBox();
         listBoxLogs = new System.Windows.Forms.ListBox();
+        checkBoxShowGlobal = new System.Windows.Forms.CheckBox();
         SuspendLayout();
+
         // 
         // listBoxClients
         // 
         listBoxClients.FormattingEnabled = true;
+        listBoxClients.HorizontalScrollbar = true;
         listBoxClients.Location = new System.Drawing.Point(12, 17);
         listBoxClients.Name = "listBoxClients";
         listBoxClients.Size = new System.Drawing.Size(244, 409);
         listBoxClients.TabIndex = 2;
         listBoxClients.SelectedIndexChanged += ListBoxClients_SelectedIndexChanged;
+
         // 
         // listBoxLogs
         // 
         listBoxLogs.FormattingEnabled = true;
+        listBoxLogs.HorizontalScrollbar = true;
         listBoxLogs.Items.AddRange(new object[] { "" });
-        listBoxLogs.Location = new System.Drawing.Point(282, 17);
+        listBoxLogs.Location = new System.Drawing.Point(262, 17);
         listBoxLogs.Name = "listBoxLogs";
-        listBoxLogs.Size = new System.Drawing.Size(506, 409);
+        listBoxLogs.Size = new System.Drawing.Size(704, 409);
         listBoxLogs.TabIndex = 3;
+
+        // 
+        // checkBoxShowGlobal
+        // 
+        checkBoxShowGlobal.Location = new System.Drawing.Point(12, 432);
+        checkBoxShowGlobal.Name = "checkBoxShowGlobal";
+        checkBoxShowGlobal.Size = new System.Drawing.Size(95, 20);
+        checkBoxShowGlobal.TabIndex = 4;
+        checkBoxShowGlobal.Text = "Global Logs";
+        checkBoxShowGlobal.UseVisualStyleBackColor = true;
+        checkBoxShowGlobal.Visible = false;
+
         // 
         // Form1
         // 
         AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
         AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        ClientSize = new System.Drawing.Size(800, 450);
+        ClientSize = new System.Drawing.Size(978, 450);
         Controls.Add(listBoxLogs);
         Controls.Add(listBoxClients);
-        Text = "Form1";
+        Location = new System.Drawing.Point(15, 15);
         ResumeLayout(false);
     }
+
+    private System.Windows.Forms.CheckBox checkBoxShowGlobal;
 
     private System.Windows.Forms.ListBox listBoxLogs;
 
--- Sharpmine.Server/Gui/Form1.cs
+++ Sharpmine.Server/Gui/Form1.cs
@@ -1,4 +1,4 @@
-using Sharpmine.Server.Protocol;
+using Sharpmine.Server.Logging;
 
 namespace Sharpmine.Server.Gui;
 
@@ -7,25 +7,86 @@ public partial class Form1 : Form
 
     private readonly Server _server;
 
-    public Form1(Server server)
+    private readonly ListLogEventSink _sink;
+
+    public Form1(Server server, ListLogEventSink sink)
     {
+        server.HandleClientsInBackground();
         _server = server;
-        _server.HandleClientsInBackground();
+        _sink = sink;
+
         InitializeComponent();
-        
-        var connectionStatusChangedAction = (ClientHandler handler) =>
+        checkBoxShowGlobal.CheckedChanged += (_, _) => RefreshLogs();
+
+        _server.ClientConnectionEstablished += _ =>
         {
-            listBoxClients.DataSource = null;
-            listBoxClients.DataSource = _server.ActiveClientHandlers.Keys; // TODO: change to .Values after the correctness of the used .Keys (RemoteEndPoint vs LocalEndPoint) was verified
+            SafeInvoke(() =>
+            {
+                listBoxClients.DataSource = null;
+
+                listBoxClients.DataSource = _server.ActiveClientHandlers.Keys
+                    .Union(_sink.ClientLogs.Keys)
+                    .Order()
+                    .ToList();
+            });
         };
 
-        _server.ClientConnectionEstablished += connectionStatusChangedAction;
-        _server.ClientConnectionTerminated += connectionStatusChangedAction;
+        _sink.GlobalLogAdded += _ =>
+        {
+            SafeInvoke(() =>
+            {
+                if (checkBoxShowGlobal.Checked)
+                {
+                    RefreshLogs();
+                }
+            });
+        };
+
+        _sink.ClientLogAdded += (id, _) =>
+        {
+            SafeInvoke(() =>
+            {
+                if (!checkBoxShowGlobal.Checked && listBoxClients.SelectedItem as string == id)
+                {
+                    RefreshLogs();
+                }
+            });
+        };
     }
 
     private void ListBoxClients_SelectedIndexChanged(object sender, EventArgs e)
     {
-        listBoxLogs.DataSource = (listBoxClients.SelectedValue as ClientHandler)?.Logs;
+        if (!checkBoxShowGlobal.Checked)
+        {
+            RefreshLogs();
+        }
+    }
+
+    private void RefreshLogs()
+    {
+        listBoxLogs.DataSource = null;
+
+        if (checkBoxShowGlobal.Checked)
+        {
+            listBoxLogs.DataSource = _sink.GlobalLogs;
+        }
+        else if (listBoxClients.SelectedItem is string id
+                 && _sink.ClientLogs.TryGetValue(id, out var clientLogList))
+        {
+            listBoxLogs.DataSource = clientLogList;
+        }
+
+        if (listBoxLogs.Items.Count > 0)
+        {
+            listBoxLogs.TopIndex = listBoxLogs.Items.Count - 1;
+        }
+    }
+
+    private void SafeInvoke(Action action)
+    {
+        if (IsDisposed) return;
+        if (InvokeRequired) BeginInvoke(action);
+        else action();
     }
 
 }

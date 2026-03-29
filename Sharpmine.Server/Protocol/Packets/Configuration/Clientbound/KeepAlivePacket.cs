6a088b417873fca57e698bb05b903e556ea48fd8
Spi3lot <spi3lot@gmail.com>
2026-03-29 22:47:33 +0200

    - Add both clientbound and serverbound KeepAlivePackets - Implement clientbound KeepAlivepacket

---
+++ Sharpmine.Server/Protocol/Packets/Configuration/Clientbound/KeepAlivePacket.cs
@@ -0,0 +1,15 @@
+﻿namespace Sharpmine.Server.Protocol.Packets.Configuration.Clientbound;
+
+public partial record KeepAlivePacket(long KeepAliveId)
+{
+
+    public Task SerializeContentAsync(
+        Stream stream,
+        BinaryWriter writer,
+        CancellationToken cancellationToken)
+    {
+        writer.Write(KeepAliveId);
+        return Task.CompletedTask;
+    }
+
+}
---
+++ Sharpmine.Server/Protocol/Packets/Configuration/Serverbound/KeepAlivePacket.cs
@@ -0,0 +1,26 @@
+﻿namespace Sharpmine.Server.Protocol.Packets.Configuration.Serverbound;
+
+public partial record KeepAlivePacket
+{
+
+    public long KeepAliveId { get; set; }
+
+    public Task DeserializeContentAsync(
+        NetworkStream stream,
+        BinaryReader reader,
+        CancellationToken cancellationToken)
+    {
+        KeepAliveId = reader.ReadInt64();
+        return Task.CompletedTask;
+    }
+
+    public Task ProcessAsync(ClientHandler handler,
+        NetworkStream stream,
+        BinaryReader reader,
+        BinaryWriter writer,
+        CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException("Check whether the client responded with the same packet");
+    }
+
+}

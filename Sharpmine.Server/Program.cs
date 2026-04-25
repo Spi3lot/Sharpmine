e98a48c61802ed42d31436001acc16442a8af88f
Spi3lot <spi3lot@gmail.com>
2026-04-25 04:26:29 +0200

    #8 Close and flush Serilog Logger
    Use bootstrap logger in case something crashes before our actual logger is even created

--- Sharpmine.Server/Program.cs
+++ Sharpmine.Server/Program.cs
@@ -16,41 +16,56 @@ public static class Program
     [STAThread]
     public static async Task Main(params string[] args)
     {
-        var builder = Host.CreateApplicationBuilder(args);
-
-        builder.AddServiceDefaults();
-        Console.WriteLine($"Current Environment: {builder.Environment.EnvironmentName}");
-
-        bool nogui = builder.Configuration.GetValue<bool>("nogui");
-        ushort port = builder.Configuration.GetValue<ushort?>("port") ?? 25565;
-        var sink = new ListLogEventSink();
-
         Log.Logger = new LoggerConfiguration()
-            .ReadFrom.Configuration(builder.Configuration)
-            .WriteTo.Sink(sink)
-            .CreateLogger();
+            .WriteTo.Console()
+            .CreateBootstrapLogger();
 
-        builder.Services.AddSerilog();
-        builder.Services.AddSingleton(sink);
-        builder.Services.AddSingleton<ClientHandlerFactory>();
-        builder.Services.AddSingleton<Form1>();
-        builder.Services.AddSingleton<ServerFactory>();
-        builder.Services.AddSingleton<ServerService>(sp => sp.GetRequiredService<ServerFactory>().Create(port));
-        builder.Services.AddHostedService<ServerService>(sp => sp.GetRequiredService<ServerService>());
-
-        using var host = builder.Build();
-
-        if (nogui || args.Contains("--nogui", StringComparer.OrdinalIgnoreCase)
-                  || args.Contains("/nogui", StringComparer.OrdinalIgnoreCase))
+        try
         {
-            await host.RunAsync();
-            return;
-        }
+            var builder = Host.CreateApplicationBuilder(args);
 
-        await host.StartAsync();
-        ApplicationConfiguration.Initialize();
-        Application.Run(host.Services.GetRequiredService<Form1>());
-        await host.StopAsync();
+            builder.AddServiceDefaults();
+            Console.WriteLine($"Current Environment: {builder.Environment.EnvironmentName}");
+
+            bool nogui = builder.Configuration.GetValue<bool>("nogui");
+            ushort port = builder.Configuration.GetValue<ushort?>("port") ?? 25565;
+            var sink = new ListLogEventSink();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Configuration)
+                .WriteTo.Sink(sink)
+                .CreateLogger();
+
+            builder.Services.AddSerilog();
+            builder.Services.AddSingleton(sink);
+            builder.Services.AddSingleton<ClientHandlerFactory>();
+            builder.Services.AddSingleton<Form1>();
+            builder.Services.AddSingleton<ServerFactory>();
+            builder.Services.AddSingleton<ServerService>(sp => sp.GetRequiredService<ServerFactory>().Create(port));
+            builder.Services.AddHostedService<ServerService>(sp => sp.GetRequiredService<ServerService>());
+
+            using var host = builder.Build();
+
+            if (nogui || args.Contains("--nogui", StringComparer.OrdinalIgnoreCase)
+                      || args.Contains("/nogui", StringComparer.OrdinalIgnoreCase))
+            {
+                await host.RunAsync();
+                return;
+            }
+
+            await host.StartAsync();
+            ApplicationConfiguration.Initialize();
+            Application.Run(host.Services.GetRequiredService<Form1>());
+            await host.StopAsync();
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "The application encountered a fatal error and crashed");
+        }
+        finally
+        {
+            await Log.CloseAndFlushAsync();
+        }
     }
 
 }

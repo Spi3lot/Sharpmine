a477f156686a81a4e507e2bdc70c00e7672c36a4
Spi3lot <spi3lot@gmail.com>
2026-05-03 23:09:13 +0200

    - Create MinecraftDateConverter in the namespace it is needed in - Move TextComponentConverter into the namespace it is needed in

---
+++ Sharpmine.Server/Security/Models/MinecraftDateConverter.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Sharpmine.Server.Security.Models;
+
+public class MinecraftDateConverter : JsonConverter<DateTimeOffset?>
+{
+
+    private const string Format = "yyyy-MM-dd HH:mm:ss Z";
+
+    public override DateTimeOffset? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        string? dateString = reader.GetString();
+
+        if (string.IsNullOrWhiteSpace(dateString) || dateString.Equals("forever", StringComparison.OrdinalIgnoreCase))
+        {
+            return null;
+        }
+
+        bool couldParse = DateTimeOffset.TryParseExact(
+            dateString,
+            Format,
+            CultureInfo.InvariantCulture,
+            DateTimeStyles.None,
+            out var date);
+
+        return (couldParse) ? date : null;
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTimeOffset? value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value?.ToString(Format) ?? "forever");
+    }
+
+}

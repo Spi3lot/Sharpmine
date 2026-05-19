e7293e6ccc8cc9823c2954e877e46e267bfc9ce8
Spi3lot <spi3lot@gmail.com>
2026-05-19 19:38:24 +0200

    Improve PreSerializedPacket
    - Change PreSerializedPacket's type from record to class
    - Remove primary ctor
    - Add private ctor
    - Add UnderlyingPacketType property
    - Add explicit improved ToString

--- Sharpmine.Server.Infrastructure/Protocol/Packets/PreSerializedPacket.cs
+++ Sharpmine.Server.Infrastructure/Protocol/Packets/PreSerializedPacket.cs
@@ -2,19 +2,40 @@ using System.Buffers;
 
 namespace Sharpmine.Server.Infrastructure.Protocol.Packets;
 
-public record PreSerializedPacket(ProtocolState State, int Id, byte[] Content) : IClientboundPacket
+public class PreSerializedPacket : IClientboundPacket
 {
 
-    public static PreSerializedPacket Generate(IClientboundPacket packet)
+    private PreSerializedPacket(Type underlyingPacketType, ProtocolState state, int id, byte[] content)
+    {
+        UnderlyingPacketType = underlyingPacketType;
+        State = state;
+        Id = id;
+        Content = content;
+    }
+
+    public Type UnderlyingPacketType { get; }
+
+    public ProtocolState State { get; }
+
+    public int Id { get; }
+
+    public byte[] Content { get; }
+
+    public static PreSerializedPacket Generate<TPacket>(TPacket packet) where TPacket : IClientboundPacket
     {
         var writer = new ArrayBufferWriter<byte>();
         packet.SerializeContent(writer);
-        return new PreSerializedPacket(packet.State, packet.Id, writer.WrittenSpan.ToArray());
+        return new PreSerializedPacket(typeof(TPacket), packet.State, packet.Id, writer.WrittenSpan.ToArray());
     }
 
     public void SerializeContent(IBufferWriter<byte> writer)
     {
         writer.Write(Content);
+    }
+
+    public override string ToString()
+    {
+        return $"Pre-Serialized {UnderlyingPacketType.Name} {{ {nameof(State)} = {State}, {nameof(Id)} = {Id} }}";
     }
 
 }

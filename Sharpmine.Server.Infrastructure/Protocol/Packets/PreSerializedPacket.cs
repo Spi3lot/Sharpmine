2df3ea63f927c466703a0e4f9c35fcd0cc0477c7
Zottel Emilio <e.zottel@pke.at>
2026-05-21 16:40:51 +0200

    Improve PreSerializedPacket by storing just one ArrayBufferWriter<byte> per thread

--- Sharpmine.Server.Infrastructure/Protocol/Packets/PreSerializedPacket.cs
+++ Sharpmine.Server.Infrastructure/Protocol/Packets/PreSerializedPacket.cs
@@ -7,17 +7,25 @@ namespace Sharpmine.Server.Infrastructure.Protocol.Packets;
 public static class PreSerializedPacket
 {
 
+    [ThreadStatic]
+    private static readonly ArrayBufferWriter<byte> ArrayBufferWriter;
+
+    static PreSerializedPacket()
+    {
+        ArrayBufferWriter = new ArrayBufferWriter<byte>();
+    }
+
     public static PreSerializedPacket<TPacket> Generate<TPacket>(TPacket packet, bool retainUnderlyingPacket = false)
         where TPacket : IClientboundPacket
     {
-        var writer = new ArrayBufferWriter<byte>();
-        packet.SerializeContent(writer);
+        ArrayBufferWriter.Clear();
+        packet.SerializeContent(ArrayBufferWriter);
 
         return new PreSerializedPacket<TPacket>(
             packet.SomeWhen(_ => retainUnderlyingPacket),
             packet.State,
             packet.Id,
-            writer.WrittenSpan.ToArray());
+            ArrayBufferWriter.WrittenSpan.ToArray());
     }
 
 }

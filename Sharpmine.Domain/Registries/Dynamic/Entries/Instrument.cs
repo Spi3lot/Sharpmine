31753fe87b45dd98db0123359bc013b1aeb783da
Spi3lot <spi3lot@gmail.com>
2026-06-05 04:13:51 +0200

    Fix type of Instrument.UseDuration

--- Sharpmine.Domain/Registries/Dynamic/Entries/Instrument.cs
+++ Sharpmine.Domain/Registries/Dynamic/Entries/Instrument.cs
@@ -4,6 +4,6 @@ namespace Sharpmine.Domain.Registries.Dynamic.Entries;
 
 public record Instrument(
     Identifier SoundEvent,
-    int UseDuration,
+    float UseDuration,
     float Range,
     TextComponent Description);

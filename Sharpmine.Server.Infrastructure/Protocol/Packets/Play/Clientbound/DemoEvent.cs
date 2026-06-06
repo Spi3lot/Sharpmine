ba52b9f51b20a0f25b038c4f5b7088a57f22729b
Spi3lot <spi3lot@gmail.com>
2026-06-06 04:20:02 +0200

    Add DemoEvent enum
    Addition to 9efcf09fd13e336255acbb16fd0fe7cad1a42795

---
+++ Sharpmine.Server.Infrastructure/Protocol/Packets/Play/Clientbound/DemoEvent.cs
@@ -0,0 +1,16 @@
+﻿namespace Sharpmine.Server.Infrastructure.Protocol.Packets.Play.Clientbound;
+
+public enum DemoEvent : byte
+{
+
+    ShowWelcomeToDemoScreen = 0,
+
+    TellMovementControls = 101,
+
+    TellJumpControl = 102,
+
+    TellInventoryControl = 103,
+
+    TellDemoOverAndPrintScreenshotTutorial = 104,
+
+}

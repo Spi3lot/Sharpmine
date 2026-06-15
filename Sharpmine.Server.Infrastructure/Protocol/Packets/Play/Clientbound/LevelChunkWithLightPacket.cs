86c893fcd830cd084366a4a10d33fc6a604d28ed
Spi3lot <spi3lot@gmail.com>
2026-06-15 23:28:35 +0200

    Add LevelChunkWithLightPacket

---
+++ Sharpmine.Server.Infrastructure/Protocol/Packets/Play/Clientbound/LevelChunkWithLightPacket.cs
@@ -0,0 +1,31 @@
+﻿using System.Buffers;
+
+using Raspite.Tags;
+
+using Sharpmine.Server.Infrastructure.Protocol.DataTypes;
+using Sharpmine.Server.Infrastructure.Protocol.Extensions;
+
+namespace Sharpmine.Server.Infrastructure.Protocol.Packets.Play.Clientbound;
+
+public partial record LevelChunkWithLightPacket(
+    int ChunkX,
+    int ChunkZ,
+    CompoundTag Heightmaps,
+    Chunk Column,
+    BlockEntity[] BlockEntities,
+    LightData LightData)
+{
+
+    public void SerializeContent(IBufferWriter<byte> writer)
+    {
+        writer.WriteInt32(ChunkX);
+        writer.WriteInt32(ChunkZ);
+        writer.WriteNbt(Heightmaps, network: true);
+        var chunkDataWriter = new ArrayBufferWriter<byte>(131_072);
+        chunkDataWriter.WriteArray(Column.Sections);
+        writer.WritePrefixed(chunkDataWriter.WrittenSpan);
+        writer.WritePrefixedArray(BlockEntities, static (writer, entity) => entity.Serialize(writer));
+        writer.Write(LightData);
+    }
+
+}

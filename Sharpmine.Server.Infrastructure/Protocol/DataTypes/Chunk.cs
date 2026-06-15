40d678fb365f55a8964f08cb02fbd24cfdb8ccae
Spi3lot <spi3lot@gmail.com>
2026-06-15 23:27:57 +0200

    Implement Chunks and Sections thereof

---
+++ Sharpmine.Server.Infrastructure/Protocol/DataTypes/Chunk.cs
@@ -0,0 +1,25 @@
+﻿using Sharpmine.Domain.Registries.Dynamic.Entries;
+
+namespace Sharpmine.Server.Infrastructure.Protocol.DataTypes;
+
+public sealed class Chunk
+{
+
+    private readonly int _minY;
+
+    public Chunk(DimensionType dimension)
+    {
+        _minY = dimension.MinY;
+        Sections = new ChunkSection[dimension.Height / 16];
+
+        for (int i = 0; i < Sections.Length; i++)
+        {
+            Sections[i] = new ChunkSection();
+        }
+    }
+
+    public ChunkSection[] Sections { get; }
+
+    public ChunkSection GetSection(int blockY) => Sections[(blockY - _minY) / 16];
+
+}
---
+++ Sharpmine.Server.Infrastructure/Protocol/DataTypes/ChunkSection.cs
@@ -0,0 +1,46 @@
+﻿using System.Buffers;
+using System.Collections.Immutable;
+
+using Sharpmine.Domain.Registries.Static;
+using Sharpmine.Server.Infrastructure.Protocol.Extensions;
+
+namespace Sharpmine.Server.Infrastructure.Protocol.DataTypes;
+
+public sealed class ChunkSection : IClientboundDataType
+{
+
+    private static readonly ImmutableArray<int> AirStateIds =
+    [
+        .. Blocks.All.Values
+            .Where(block => block.Type == BlockTypes.Air)
+            .SelectMany(block => block.States)
+            .Select(state => state.Id)
+    ];
+
+    public short BlockCount { get; private set; }
+
+    public PalettedContainer BlockStates { get; } = new(PalettedContainerType.BlockStates);
+
+    public PalettedContainer Biomes { get; } = new(PalettedContainerType.Biomes);
+
+    public void SetBlock(int x, int y, int z, int stateId)
+    {
+        int oldStateId = BlockStates.Get(x, y, z);
+        if (oldStateId == stateId) return;
+
+        BlockStates.Set(x, y, z, stateId);
+        bool wasAir = AirStateIds.Contains(oldStateId);
+        bool isAir = AirStateIds.Contains(stateId);
+
+        if (wasAir && !isAir) BlockCount++;
+        else if (!wasAir && isAir) BlockCount--;
+    }
+
+    public void Serialize(IBufferWriter<byte> writer)
+    {
+        writer.WriteInt16(BlockCount);
+        BlockStates.Serialize(writer);
+        Biomes.Serialize(writer);
+    }
+
+}

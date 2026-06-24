82dc77098a5f555b8eee50b3c9e9f629a009009d
Spi3lot <spi3lot@gmail.com>
2026-06-24 23:38:44 +0200

    Make Data array public in Heightmap

--- Sharpmine.Server.Infrastructure/Protocol/DataTypes/Heightmap.cs
+++ Sharpmine.Server.Infrastructure/Protocol/DataTypes/Heightmap.cs
@@ -8,12 +8,12 @@ namespace Sharpmine.Server.Infrastructure.Protocol.DataTypes;
 public readonly record struct Heightmap(HeightmapType Type) : IClientboundDataType
 {
 
-    private readonly long[] _data = [];
+    public long[] Data { get; init; }
 
     public void Serialize(IBufferWriter<byte> writer)
     {
         writer.WriteVarInt((int) Type);
-        writer.WritePrefixedArray(_data, static (writer, @long) => writer.WriteInt64(@long));
+        writer.WritePrefixedArray(Data, static (writer, @long) => writer.WriteInt64(@long));
     }
 
 }

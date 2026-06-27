2338094aebca31b18b4e85e7f982ffd2bf50c5cb
Spi3lot <spi3lot@gmail.com>
2026-06-27 11:44:55 +0200

    Add `ClientTickEndPacketHandler` to mute "Missing handler" logs

---
+++ Sharpmine.Server.Infrastructure/Protocol/Handlers/Play/ClientTickEndPacketHandler.cs
@@ -0,0 +1,13 @@
+﻿using Sharpmine.Server.Infrastructure.Protocol.Packets.Play.Serverbound;
+
+namespace Sharpmine.Server.Infrastructure.Protocol.Handlers.Play;
+
+public class ClientTickEndPacketHandler : IPacketHandler<ClientTickEndPacket>
+{
+
+    public ValueTask HandleAsync(
+        ClientTickEndPacket packet,
+        ClientHandler client,
+        CancellationToken cancellationToken) => ValueTask.CompletedTask;
+
+}

b90d35a6c293358b0c94510972172c176cede76d
Spi3lot <spi3lot@gmail.com>
2026-07-04 13:56:20 +0200

    Add chunk JIT warmup to `ServerBootstrapService`

--- Sharpmine.Server.Infrastructure/ServerBootstrapService.cs
+++ Sharpmine.Server.Infrastructure/ServerBootstrapService.cs
@@ -2,8 +2,10 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+using Sharpmine.Domain.DataTypes;
 using Sharpmine.Domain.Registries.Static;
 using Sharpmine.Domain.Tags;
+using Sharpmine.Server.Domain.Registries.Dynamic;
 using Sharpmine.Server.Infrastructure.Configuration;
 using Sharpmine.Server.Infrastructure.Protocol;
 
@@ -11,6 +13,7 @@ namespace Sharpmine.Server.Infrastructure;
 
 public class ServerBootstrapService(
     DatapackLoader datapackLoader,
+    IRegistries registries,
     IServiceProvider serviceProvider,
     ILogger<ServerBootstrapService> logger) : IHostedService
 {
@@ -20,12 +23,11 @@ public class ServerBootstrapService(
         await datapackLoader.ReloadAllAsync();
         logger.LogInformation("Dynamic registries filled successfully");
 
-        // TODO: Add more registries to speed up initial connection even further
-        _ = Blocks.Air;
         _ = BlockTags.Air;
+        new Chunk(registries.DimensionTypes["overworld"]).SetBlock(0, 0, 0, Blocks.Air.DefaultState.Id);
         serviceProvider.GetRequiredService<NetworkRegistryCache>();
         serviceProvider.GetRequiredService<NetworkTagCache>();
-        logger.LogInformation("Registry caches preloaded successfully");
+        logger.LogInformation("JIT and registry caches warmed up successfully");
     }
 
     public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;

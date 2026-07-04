ebd2379107461dd35f105c099de906e7b80716cb
Spi3lot <spi3lot@gmail.com>
2026-07-04 20:01:49 +0200

    Add `MovePlayerStatusOnlyPacket`

---
+++ Sharpmine.Server.Infrastructure/Protocol/Packets/Play/Serverbound/MovePlayerStatusOnly.cs
@@ -0,0 +1,19 @@
+using System.Buffers;
+
+using Sharpmine.Domain.Extensions;
+using Sharpmine.Server.Infrastructure.Protocol.Attributes;
+
+namespace Sharpmine.Server.Infrastructure.Protocol.Packets.Play.Serverbound;
+
+public partial record MovePlayerStatusOnlyPacket
+{
+
+    [PacketProperty]
+    private MovementStates _flags;
+
+    public bool DeserializeContent(ref SequenceReader<byte> reader)
+    {
+        return reader.TryReadEnum<MovementStates, byte>(out _flags, static (ref reader, out value) => reader.TryReadByte(out value));
+    }
+
+}

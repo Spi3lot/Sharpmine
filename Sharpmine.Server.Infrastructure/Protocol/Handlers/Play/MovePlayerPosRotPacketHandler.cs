71c128e1458298e0db5be5c17feeafb3f068fb96
Spi3lot <spi3lot@gmail.com>
2026-07-05 01:54:20 +0200

    Add handlers for `MovePlayerRotPacket` and `MovePlayerPosRotPacket`

---
+++ Sharpmine.Server.Infrastructure/Protocol/Handlers/Play/MovePlayerPosRotPacketHandler.cs
@@ -0,0 +1,19 @@
+﻿using Sharpmine.Domain.DataTypes;
+using Sharpmine.Server.Infrastructure.Protocol.Packets.Play.Serverbound;
+
+namespace Sharpmine.Server.Infrastructure.Protocol.Handlers.Play;
+
+public class MovePlayerPosRotPacketHandler : IPacketHandler<MovePlayerPosRotPacket>
+{
+
+    public ValueTask HandleAsync(
+        MovePlayerPosRotPacket packet,
+        ClientHandler client,
+        CancellationToken cancellationToken)
+    {
+        client.Player!.Position = new PlayerPosition(packet.X, packet.FeetY, packet.Z);
+        client.Player!.Rotation = new PlayerRotation(packet.Yaw, packet.Pitch);
+        return ValueTask.CompletedTask;
+    }
+
+}
---
+++ Sharpmine.Server.Infrastructure/Protocol/Handlers/Play/MovePlayerRotPacketHandler.cs
@@ -0,0 +1,18 @@
+﻿using Sharpmine.Domain.DataTypes;
+using Sharpmine.Server.Infrastructure.Protocol.Packets.Play.Serverbound;
+
+namespace Sharpmine.Server.Infrastructure.Protocol.Handlers.Play;
+
+public class MovePlayerRotPacketHandler : IPacketHandler<MovePlayerRotPacket>
+{
+
+    public ValueTask HandleAsync(
+        MovePlayerRotPacket packet,
+        ClientHandler client,
+        CancellationToken cancellationToken)
+    {
+        client.Player!.Rotation = new PlayerRotation(packet.Yaw, packet.Pitch);
+        return ValueTask.CompletedTask;
+    }
+
+}

47a28b9e5e22c668cd91ccedd6bbda53afee3be8
Spi3lot <spi3lot@gmail.com>
2026-07-04 20:27:19 +0200

    Add `ChangeGameModePacket` and `ChangeGameModePacketHandler`

---
+++ Sharpmine.Server.Infrastructure/Protocol/Handlers/Play/ChangeGameModePacketHandler.cs
@@ -0,0 +1,19 @@
+﻿using Sharpmine.Server.Infrastructure.Protocol.Packets.Play.Clientbound;
+using Sharpmine.Server.Infrastructure.Protocol.Packets.Play.Serverbound;
+
+namespace Sharpmine.Server.Infrastructure.Protocol.Handlers.Play;
+
+public class ChangeGameModePacketHandler : IPacketHandler<ChangeGameModePacket>
+{
+
+    public ValueTask HandleAsync(
+        ChangeGameModePacket packet,
+        ClientHandler client,
+        CancellationToken cancellationToken)
+    {
+        // TODO: Secure
+        client.SendPacket(GameEventPacket.ChangeGameMode(packet.GameMode));
+        return ValueTask.CompletedTask;
+    }
+
+}
---
+++ Sharpmine.Server.Infrastructure/Protocol/Packets/Play/Serverbound/ChangeGameModePacket.cs
@@ -0,0 +1,20 @@
+using System.Buffers;
+
+using Sharpmine.Domain.DataTypes;
+using Sharpmine.Domain.Extensions;
+using Sharpmine.Server.Infrastructure.Protocol.Attributes;
+
+namespace Sharpmine.Server.Infrastructure.Protocol.Packets.Play.Serverbound;
+
+public partial record ChangeGameModePacket
+{
+
+    [PacketProperty]
+    private GameMode _gameMode;
+
+    public bool DeserializeContent(ref SequenceReader<byte> reader)
+    {
+        return reader.TryReadEnum(out _gameMode);
+    }
+
+}

5e9f6cd3640ed25d8e93dcbf6c71b33f0a6481a5
Spi3lot <spi3lot@gmail.com>
2026-07-04 20:01:21 +0200

    Add `MovePlayerPosPacketHandler`

---
+++ Sharpmine.Server.Infrastructure/Protocol/Handlers/Play/MovePlayerPosPacketHandler.cs
@@ -0,0 +1,18 @@
+﻿using Sharpmine.Domain.DataTypes;
+using Sharpmine.Server.Infrastructure.Protocol.Packets.Play.Serverbound;
+
+namespace Sharpmine.Server.Infrastructure.Protocol.Handlers.Play;
+
+public class MovePlayerPosPacketHandler : IPacketHandler<MovePlayerPosPacket>
+{
+
+    public ValueTask HandleAsync(
+        MovePlayerPosPacket packet,
+        ClientHandler client,
+        CancellationToken cancellationToken)
+    {
+        client.Player!.Position = new PlayerPosition(packet.X, packet.FeetY, packet.Z);
+        return ValueTask.CompletedTask;
+    }
+
+}
